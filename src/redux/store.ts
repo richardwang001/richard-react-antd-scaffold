02f240a802ceb9ec61c8ea326977c7ac50a8c6a7
Richard <1782317677@qq.com>
2022-08-01 17:42:50 +0800

    use redux-persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,14 +4,41 @@
  *@date 2022/7/25 13:31
  */
 
-import { configureStore } from '@reduxjs/toolkit'
-import menuReducer from '../menu/menuSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const store = configureStore({
-  reducer: {
-    menu:menuReducer
-  },
-})
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    }),
+});
+
+let persistor = persistStore(store);
+
+export { store, persistor };
 
 export type RootState = ReturnType<typeof store.getState>
 export type DispatchType = typeof store.dispatch
